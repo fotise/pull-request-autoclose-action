7c58a4a3eff1ba281c58b5ec9d37699059412db5
fotise <58773095+fotise@users.noreply.github.com>
2020-02-25 12:09:11 +0100

    Added comment to PR and close PR

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,41 @@
+/* eslint-disable camelcase */
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as githubNS from '@actions/github'
+import {Context} from '@actions/github/lib/context'
+//const github = require('@actions/github');
+
+// import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting for ${ms} milliseconds ...`)
+    const token: string = core.getInput('github-token')
+    const message: string = core.getInput('message')
 
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
+
+    const context: Context = githubNS.context
+    if (context.payload.pull_request == null) {
+      core.debug('No pull request found.')
+      return
+    }
+    const prNumber: number = context.payload.pull_request.number
+
+    const github: githubNS.GitHub = new githubNS.GitHub(token)
+
+    github.issues.createComment({
+      ...context.repo,
+      issue_number: prNumber,
+      body: message
+    })
+    core.debug(`Created comment: ${message}`)
+
+    await github.pulls.update({
+      ...context.repo,
+      pull_number: prNumber,
+      state: 'closed'
+    })
+    core.debug(`Closed pull request: ${prNumber}`)
+
     core.debug(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
